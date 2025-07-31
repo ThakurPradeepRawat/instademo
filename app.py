33932871b241c2633156cf4e60cfa20e830b625c
Thakur Pradeep rawat <P.s.rawat4458@gmail.com>
2025-07-31 15:12:03 +0530

    commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def login():
     return render_template('insta.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
